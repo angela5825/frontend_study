656916afcbaaa2eb0dff8985cfb69c176c277838
angela5825 <99yoolee@naver.com>
2025-10-09 15:22:34 +0900

    ✨ 리엑트 상태 변화(훅) 기본 사용법 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import "./App.css";
-import Button from "./Button";
-import Input from "./Input";
+import { useState } from "react";
+
+// const App = () => {
+//   // let count = 0;
+//   const [count, setCount] = useState(0);
+
+//   // 내부적으로 배열을 반환을 하는데 변수의 값과 그 변수의 값을 변경시키는 함수
+//   return (
+//     <>
+//       <h2>Count : {count}</h2>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         +
+//       </button>
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         -
+//       </button>
+//     </>
+//   );
+// };
+
 const App = () => {
+  // let isPinned = true;
+  const [isPinned, setPin] = useState(false);
+  const [count, setCount] = useState(0);
   return (
     <>
-      <Button name={"Home"} />
-      <Button name={"Store"} />
-      <Button name={"Contact"} />
-      <br />
-      <Input></Input>
+      <h2>Count : {count}</h2>
+      <button
+        onClick={() => {
+          setCount((c) => c + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount((c) => c - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setPin((p) => !p);
+          // console.log({ isPinned });
+        }}
+      >
+        {isPinned && "📌"} Pinn This!
+      </button>
     </>
   );
 };
