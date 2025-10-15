4fe4b38c86baa6d0b22ed8afe317002d85df33b7
angela5825 <99yoolee@naver.com>
2025-10-15 18:42:39 +0900

    ✨ 리엑트 컴포넌트 생명주기 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
+import { useState } from "react";
 import "./App.css";
-import { useRef } from "react";
-
+import ClassComp from "./ClassComp";
+import FuncComp from "./FuncComp";
 const App = () => {
-  const inputRef = useRef(null); // DOM이랑 연결할 때는 null 로 선언 한다
-  const divRef = useRef(null);
-  const handleFocus = () => {
-    // console.log("helo");
-    // document.getElementById("focusInput").focus();
-    // 리엑트에서는 getElement 를 잘 이용하지 않는다..
-    // 아이디가 다른 데에서 겹칠 수도 있기 때문에
-    // useRef에서 DOM //
-    console.log(inputRef.current);
-    inputRef.current.focus();
-    divRef.current.style.width = "200px";
-    divRef.current.style.height = "200px";
-    divRef.current.style.backgroundColor = "green";
-  };
+  // 변수로 어떤 게 선택되었는 지 저장하는 변수를 선언
+
+  const [selected, setSelected] = useState("");
 
   return (
-    <div>
-      <input ref={inputRef} type="text" placeholder="Type..." />
-      <button onClick={handleFocus}>Focus Input</button>
+    <>
+      {["", "ClassComp", "FuncComp"].map((option) => (
+        <label key={option}>
+          <input
+            type="radio"
+            value={option}
+            onChange={(e) => setSelected(e.target.value)}
+            checked={selected === option}
+          />
+          {option || "None"}
+        </label>
+      ))}
+      {/* 를 map을 이용해서 loop를 돌림 */}
 
-      <div ref={divRef} onClick={handleFocus} style={{ pointer: " cursor" }}>
-        이것은 초록색 200x200 크기를 가진 박스로 변경됩니다.
-      </div>
-    </div>
+      {/* 공백이 아닐 경우 오른쪽에 */}
+      {selected && (selected === "ClassComp" ? <ClassComp /> : <FuncComp />)}
+    </>
   );
 };
 export default App;
