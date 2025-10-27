e61c5a348342bc5d79129209a0af9335df1c7ce6
angela5825 <99yoolee@naver.com>
2025-10-27 22:08:39 +0900

    ✨ 화면 사이즈 감지 커스텀 훅 기능 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { delay } from "./delay";
-import Timer from "./Timer";
+import useWindowSize from "./hooks/useWindowSize";
 
 const App = () => {
-  const [showTimer, setShowTimer] = useState(false);
+  // 과제 1. useState 훅을 이용하여 count의 값을 증가시키고 감소시키는 기능을 작성하세요
+  // 과제 2. 화면의 사이즈가 변경되었을 때 변경된 가로 새로 크기를 화면에 표시되는 기능을 작성하세요
+  const { width, height } = useWindowSize();
 
   return (
     <>
-      <label>
-        <input
-          type="checkbox"
-          checked={showTimer}
-          onChange={(e) => setShowTimer(e.target.checked)}
-        />
-        Show Timer
-      </label>
-      {showTimer && <Timer />}
+      <h2>화면의 사이즈 </h2>
+      <h3> 가로 크기 : {width}</h3>
+      <h3>세로 크기 : {height}</h3>
     </>
   );
 };
