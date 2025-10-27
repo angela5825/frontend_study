4c4a82b91dd36a94bbf012f538578b731f6d4a0d
angela5825 <99yoolee@naver.com>
2025-10-27 16:06:34 +0900

    ✨ useEffect 타이머 기능 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { delay } from "./delay";
+import Timer from "./Timer";
 
 const App = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true); // 데이터를 잘 가지고 오는 지 체크용
+  const [showTimer, setShowTimer] = useState(false);
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        await delay(500);
-        const response = await fetch("/data/books.json");
-        const data = await response.json();
-        setBooks(data);
-
-        console.log("load Complete");
-      } catch (error) {
-        console.error("Failed to fetch books", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchBooks();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
   return (
     <>
-      <div>
-        <h2>Book List</h2>
-        <ul>
-          {books.map((book) => {
-            return (
-              <li key={book.id}>
-                <strong>{book.title}</strong> by {book.author}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <label>
+        <input
+          type="checkbox"
+          checked={showTimer}
+          onChange={(e) => setShowTimer(e.target.checked)}
+        />
+        Show Timer
+      </label>
+      {showTimer && <Timer />}
     </>
   );
 };
